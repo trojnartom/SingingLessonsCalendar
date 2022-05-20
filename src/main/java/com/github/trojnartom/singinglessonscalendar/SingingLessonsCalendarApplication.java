c3d4fd499839af012bd24f374e69ef307e9244ac
trojnartom <trojnar.t@hotmail.com>
2022-05-20 13:06:23 +0200

    Initial commit

---
+++ src/main/java/com/github/trojnartom/singinglessonscalendar/SingingLessonsCalendarApplication.java
@@ -0,0 +1,13 @@
+package com.github.trojnartom.singinglessonscalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingingLessonsCalendarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SingingLessonsCalendarApplication.class, args);
+    }
+
+}
