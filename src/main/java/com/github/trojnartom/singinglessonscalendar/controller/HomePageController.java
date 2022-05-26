7ff78fb16ae5b0ed12df5270de161d5c6c04566c
trojnartom <trojnar.t@hotmail.com>
2022-05-26 12:29:23 +0200

    test2

--- src/main/java/com/github/trojnartom/singinglessonscalendar/controller/HomePageController.java
+++ src/main/java/com/github/trojnartom/singinglessonscalendar/controller/HomePageController.java
@@ -2,13 +2,14 @@ package com.github.trojnartom.singinglessonscalendar.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomePageController {
 
-    @GetMapping("/home")
+    @GetMapping("/")
     public String homePageView() {
-        return "home";
+        return "test";
     }
 }
